5383223bc5dcbed4585173c60057424b7a1a8d98
Jason Land <wishinghand@gmail.com>
2016-09-12 17:15:01 -0700

    Updating student info works

--- client/src/js/student/student.detail.controller.js
+++ client/src/js/student/student.detail.controller.js
@@ -5,18 +5,35 @@
         .module('app')
         .controller('StudentDetailCtrl', StudentDetailCtrl);
 
-    StudentDetailCtrl.$inject = ['$stateParams', '$state'];
+    StudentDetailCtrl.$inject = ['$stateParams', '$state', 'StudentFactory'];
 
     /* @ngInject */
-    function StudentDetailCtrl($stateParams, $state) {
+    function StudentDetailCtrl($stateParams, $state, StudentFactory) {
         var vm = this;
         vm.title = 'StudentDetailCtrl';
+        vm.student = {};
+        vm.studentCurrent = {};
+        vm.editStudent = editStudent;
 
-        activate();
+        getStudentDetail();
 
         ////////////////
 
-        function activate() {
+        function getStudentDetail() {
+            var studentId = $stateParams.studentId;
+            StudentFactory.readById(studentId).then(
+                function(student){
+                    vm.studentCurrent = student;
+                });
+        }
+
+        function editStudent(){
+            vm.studentNew.studentId = vm.studentCurrent.studentId;
+            StudentFactory.update(vm.studentNew).then(
+                function(studentInfo){
+                    vm.studentCurrent = studentInfo;
+                    $('.ui.basic.modal').modal('show');
+                });
         }
     }
 })();
